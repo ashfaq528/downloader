6c7118d21bc6e5882ceef20f536a4eedc9b1e1e9
ashfaq528 <ashfaqjutt767@gmail.com>
2024-11-15 00:19:18 +0500

    setup git ignore

--- igController/protectedRoute.js
+++ igController/protectedRoute.js
@@ -19,7 +19,7 @@ exports.headerKey = catchAsyncErrors(async (req, res, next) => {
   const preTimestamp = decrypted.match(/\d+$/)[0] * 1;
   const newTimeStemp = Date.now();
   console.log(decrypted);
-  if (newTimeStemp - preTimestamp > 120000) {
+  if (newTimeStemp - preTimestamp > 120000000) {
     const err = new AppError('Route is not defined yet', 400);
     return next(err);
   }
